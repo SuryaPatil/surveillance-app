5603fac59be442fabbb2402f541ed4bfa2aa0177
Surya Patil <patil.surya01@gmail.com>
2024-06-24 17:37:43 -0400

    wrote digitizer function

---
+++ build/CMakeFiles/CaptureFrames.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CaptureFrames.dir/CaptureFrames.cpp.o"
+  "CMakeFiles/CaptureFrames.dir/CaptureFrames.cpp.o.d"
+  "CaptureFrames"
+  "CaptureFrames.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CaptureFrames.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
